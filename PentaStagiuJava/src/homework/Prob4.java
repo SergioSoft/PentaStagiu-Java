387352a19f444bd5d430ebcf704f00884bc0e20f
Sergios <Sergios@Sergios-PC>
2018-12-08 23:49:15 +0200

    Problema 4

---
+++ PentaStagiuJava/src/homework/Prob4.java
@@ -0,0 +1,20 @@
+package homework;
+/*
+ Write a java program that will test whether the first and the last element of an int array are the same. We can assume the array has 2 or more elements.
+
+For example:
+For the following input [13,6,2,8,2,3] the output should be : FALSE
+For the following input [13,6,2,8,2,13] the output should be : TRUE
+ */
+public class Prob4 {
+	public static void main(String[] args) {
+		int arr[] ={13,6,2,8,2,3};    			      	
+        		if  (arr[0] == arr[arr.length -1])  {
+        			 System.out.println("TRUE");
+        			 
+        		} else {
+        			 System.out.println("FALSE");          			
+        		}  
+        	}
+        
+	}
