86fa46351fb3711a7d776d5f63b16c164e67bd7c
Sergios <Sergios@Sergios-PC>
2018-12-08 23:47:07 +0200

    Problema 9

---
+++ PentaStagiuJava/src/homework/Prob9.java
@@ -0,0 +1,30 @@
+package homework;
+/*
+ Write a program that will return a different string representing the alphabet index of each letter.
+
+Assuming we have the following alphabet encoding
+
+a = 0
+b = 1
+c = 2
+d = 3
+…
+
+For the given string “abcd” the output should be “0123”
+
+ */
+public class Prob9 {
+
+	public static void main(String[] args) {
+		   String str = "abcd";
+		   StringBuilder result = new StringBuilder("");
+
+	        for( char c : str.toCharArray() )
+	        {
+	            result.append( c - 'a' );
+	        }
+
+	        System.out.println(result.toString());
+		}
+
+	} 
