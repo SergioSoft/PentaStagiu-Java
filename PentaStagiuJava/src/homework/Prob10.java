5ad9c8f12aa6bab7c06c824dac85888af224a426
Sergios <Sergios@Sergios-PC>
2018-12-09 00:08:49 +0200

    -

---
+++ PentaStagiuJava/src/homework/Prob10.java
@@ -0,0 +1,23 @@
+package homework;
+
+/*
+Write a program that test if a given String contains another String.
+
+For example
+Given the following base String “Hello java world” and the target test String “java” the output should be TRUE
+ */
+public class Prob10 {
+
+	public static void main(String[] args) {
+		String str = "Hello java world";
+		String strTarget = "java";
+			if (str.contains(strTarget)) {
+					System.out.println("True");
+			} else {
+					System.out.println("False");
+			}
+		
+
+	}
+
+}
