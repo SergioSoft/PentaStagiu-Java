8ff42fc8657ce6e094f3d449df721a2d8a6561c3
Sergios <Sergios@Sergios-PC>
2018-12-09 00:21:22 +0200

    Problema 12

---
+++ PentaStagiuJava/src/homework/Prob12.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Prob12 {
+
+	public static void main(String[] args) {
+		String str = "Hello java world";
+		String strTarget = "orld";
+			if (str.endsWith(strTarget)) {
+				System.out.println("TRUE");				
+			}else {
+				System.out.println("FALSE");
+			}
+		
+
+	}
+
+}
