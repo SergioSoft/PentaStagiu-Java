fe3233b38fdccd0f1d1f662eba0c7e5245960e41
Sergios <Sergios@Sergios-PC>
2018-12-09 00:11:43 +0200

    Problema 1

---
+++ PentaStagiuJava/src/homework/Prob1.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class Prob1 {
+	  public static void main(String args[]) {
+			int myTarget = 5;
+
+			int res = 0;
+			for (int i = 0; i <=myTarget; i ++) {
+				res = res  + i;
+			}
+
+
+			System.out.println("Sum is " + res);
+	  }
+
+}
+ 
+	    	
+	
