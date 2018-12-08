6828fd19cf7c54335bee72620fed8f6a32553df6
Sergios <Sergios@Sergios-PC>
2018-12-09 00:19:05 +0200

    Problema 11

---
+++ PentaStagiuJava/src/homework/Prob11.java
@@ -0,0 +1,24 @@
+package homework;
+/*
+Write a program that will count how many times a character appears in a String
+
+For example
+Given the string “Hello java world” and the target character ‘a’ the output should be 2 
+(since ‘a’ appears twice in “Hello java world”)
+*/
+public class Prob11 {
+
+	public static void main(String[] args) {
+		String str = "Hello java world";
+		String strTarget = "a";
+		int count  = 0;
+			for (int i=0; i<str.length(); i++ ) {
+				if(str.charAt(i) == strTarget.charAt(0)) {
+					count++;
+					
+				}
+			}
+		System.out.println(count);
+	}
+
+}
